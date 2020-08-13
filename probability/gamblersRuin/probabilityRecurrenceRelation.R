9c37651410882b58aae70e48469083a5cc3b5f43
vvasuki <vishvas.vasuki@gmail.com>
2020-08-13 17:47:59 +0530

    CU

--- probability/gamblersRuin/probabilityRecurrenceRelation.R
+++ probability/gamblersRuin/probabilityRecurrenceRelation.R
@@ -1,5 +1,18 @@
-getCapitalProbabilities = function(turn, initCapitalX) {
-  #if (turn < initCapitalX) {
-  #  return init
-  #}
+x.p = 0.495
+x.capital = 25
+y.capital = 10000
+maxCapital = x.capital + y.capital
+
+getCapitalProbabilities = function(prevCapitalProbabilities) {
+  capitalProbabilities = rep(0, maxCapital + 1)
+  for (i in seq(maxCapital + 1)) {
+    if(i < maxCapital) {
+      capitalProbabilities[i] = prevCapitalProbabilities[i+1]*(1-x.p)
+    }
+    if(i > 1) {
+      capitalProbabilities[i] = capitalProbabilities[i] + prevCapitalProbabilities[i-1]*x.p
+    }
+  }
+  return(capitalProbabilities)
 }
+
